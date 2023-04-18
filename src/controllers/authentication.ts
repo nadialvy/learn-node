d0fde091843690d12b2d1a7b887a6114df16c359
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-04-18 15:31:03 +0700

    create routing and fix some import

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,27 +1,30 @@
 import express from 'express';
-import { getUserByEmail, createUser } from 'db/users';
-import { random, passAuthentication } from 'helpers/index';
+import { getUserByEmail, createUser } from '../db/users';
+import { random, passAuthentication } from '../helpers/index';
 
 export const register = async (req: express.Request, res: express.Response) => {
   try{
     const {email, password, username} = req.body;
 
     if(!email || !password || !username){
-      return res.sendStatus(400);
+      return res.status(400).json({
+        message: "Email, password, and username are required"
+      });
     }
 
     const existingUser = await getUserByEmail(email);
     if(existingUser){
-      return res.sendStatus(400)
+      return res.status(400).json({
+        message: "Email already exists"
+      });
     }
 
     const salt = random();
-    const userPassword = passAuthentication(salt, password);
     const user = await createUser({
       username,
       email,
       authentication: {
-        password: userPassword,
+        password: passAuthentication(salt, password),
         salt: salt
       }
     });
@@ -29,6 +32,8 @@ export const register = async (req: express.Request, res: express.Response) => {
     return res.status(200).json(user).end();
   }catch (error){
     console.log(error);
-    return res.sendStatus(400);
+    return res.status(500).json({
+      message: "Internal server error"
+    });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import router from '../src/router/index';
 
 const app = express();
 
@@ -29,3 +30,4 @@ mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error));
 
+app.use('/', router());
