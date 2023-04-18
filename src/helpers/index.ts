2d7b82abda8d0581823171572ccaa5b772686afc
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-04-18 14:40:51 +0700

    auth controller

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,8 +3,7 @@ import crypto from 'crypto';
 const SECRET = 'NADIA-REST-API';
 
 export const random = () => crypto.randomBytes(128).toString('base64');
-export const authentication = (salt: string, password: string) => {
+
+export const passAuthentication = (salt: string, password: string) => {
   return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET).digest()
 }
-
-
