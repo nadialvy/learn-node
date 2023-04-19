4528b03f9e42819888a7a53ba6fa0d88ad57f9fa
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-04-19 19:38:04 +0700

    create isOwner middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,6 +3,33 @@ import { get, merge } from 'lodash';
 
 import { getUserBySessionToken } from '../db/users';
 
+export const isOwner = async (req: express.Request, res: express.Response,next: express.NextFunction) => {
+  try{
+    const { id } = req.params;
+    const currentUser = get(req, 'identity._id') as string;
+
+    if(!currentUser){
+      return res.status(403).json({
+        message: "Forbidden, please login first"
+      })
+    }
+
+    if(currentUser.toString() !== id){
+      return res.status(403).json({
+        message: "You are not allowed"
+      });
+    }
+
+    return next();
+
+  } catch (error){
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error"
+    });
+  }
+}
+
 export const isAuthenticated = async (req: express.Request, res: express.Response,next: express.NextFunction) => {
   try{
     const sessionToken = req.cookies['NADIA-AUTH'];
@@ -21,7 +48,7 @@ export const isAuthenticated = async (req: express.Request, res: express.Respons
       });
     }
 
-    merge(req, { identity: existingUser });
+    merge(req, { identity: await existingUser });
 
     return next();
 
