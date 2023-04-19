84a3c8a11c9937c22f202724b1fa2352e83d4ec2
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-04-19 13:16:53 +0700

    middleware

---
+++ src/middlewares/index.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import { get, merge } from 'lodash';
+
+import { getUserBySessionToken } from '../db/users';
+
+export const isAuthenticated = async (req: express.Request, res: express.Response,next: express.NextFunction) => {
+  try{
+    const sessionToken = req.cookies['NADIA-AUTH'];
+
+    if(!sessionToken){
+      return res.send(403).json({
+        message: "Token is not detected"
+      });
+    }
+
+    const existingUser = getUserBySessionToken(sessionToken);
+
+    if(!existingUser){
+      return res.send(403).json({
+        message: "User not found"
+      });
+    }
+
+    merge(req, { identity: existingUser });
+
+    return next();
+
+  } catch (error){
+    console.log(error);
+    return res.send(500).json({
+      message: "Internal server error"
+    })
+  }
+}
