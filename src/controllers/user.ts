aaaae921e8393444db4e6632de1a8dcb2467cfd2
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-04-19 15:08:47 +0700

    delete user

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getUsers } from '../db/users';
+import { getUsers,getUserById, deleteUserById } from '../db/users';
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
   try{
@@ -10,6 +10,42 @@ export const getAllUsers = async (req: express.Request, res: express.Response) =
   } catch (error){
     console.log(error);
     return res.status(500).json({
+      message: "Internal server error"
+    });
+  }
+}
+
+export const deleteUser = async (req: express.Request, res: express.Response) => {
+  try{
+    const _id = req.body;
+
+    if(!_id){
+      return res.status(400).json({
+        message: "Id is required"
+      });
+    }
+
+    const existingUser = await getUserById(_id);
+
+    if(!existingUser){
+      return res.status(400).json({
+        message: "User not foud, try correcting id"
+      });
+    }
+
+    const deleteUser = await deleteUserById(_id);
+    const updatedUser = await getUsers();
+
+    if(deleteUser){
+      return res.status(200).json({
+        message: "Success delete user",
+        remainingUser: updatedUser
+      })
+    }
+
+  } catch (error){
+    console.log(error);
+    res.status(500).json({
       message: "Internal server error"
     });
   }
